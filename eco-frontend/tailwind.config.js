34367cbc300cd128e9b3175e6432945c5f343aaf
sarai <jf25001@itcha.edu.sv>
2026-05-13 21:00:03 -0600

    Proyecto inicial

---
+++ eco-frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  // AÑADE ESTO: Es lo que evita que los botones de primevue pierdan el fondo
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {
+      colors:{
+        primary: "#0F172A",
+        secondary: "#1E293B",
+        accent: "#3B82F6",
+      }
+    },
+  },
+  plugins: [],
+}
---
+++ economica-backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/saludo', function () {
+    return response()->json([
+        'mensaje' => 'Hola desde Laravel'
+    ]);
+});
