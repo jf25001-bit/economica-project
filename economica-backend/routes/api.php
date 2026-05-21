7bf77019e261981d92fdc782fa8ca849c9fd206b
RobertoACastillo <ca25003@itcha.edu.sv>
2026-05-21 07:28:46 -0600

    registro de usuarios y login

--- economica-backend/routes/api.php
+++ economica-backend/routes/api.php
@@ -2,9 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/saludo', function () {
     return response()->json([
         'mensaje' => 'Hola desde Laravel'
     ]);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::prefix('auth')->group(function(){
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:api')->group(function(){
+        Route::get('me',[AuthController::class, 'me']);
+        Route::post('logout',[AuthController::class, 'logout']);
+        Route::post('refresh',[AuthController::class, 'refresh']);
+    });
 });
